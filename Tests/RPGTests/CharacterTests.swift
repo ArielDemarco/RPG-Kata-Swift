57294feb3a34f5a33496becdb1307c7bcf694f28
Ariel Demarco <demarcoariel.dev@gmail.com>
2021-04-24 02:52:22 -0300

    Finished iteration 1

--- Tests/RPGTests/CharacterTests.swift
+++ Tests/RPGTests/CharacterTests.swift
@@ -8,15 +8,39 @@
 import XCTest
 
 struct RPGCharacter {
-    let health: Int
-    let level: Int
+    private struct Constants {
+        static let initialHealth: Int = 1000
+        static let maximumHealth: Int = 1000
+    }
+    private(set) var health: Int
+    private(set) var level: Int
     var isAlive: Bool {
         health > 0
     }
     
-    init(health: Int = 1000, level: Int = 1) {
+    init(health: Int = Constants.initialHealth, level: Int = 1) {
         self.health = health
         self.level = level
+    }
+    
+    mutating func receiveDamage(of damageAmount: Int) {
+        health = max(health - damageAmount, 0)
+    }
+    
+    mutating func receiveHealing(of healingAmount: Int) {
+        health = min(health + healingAmount, Constants.maximumHealth)
+    }
+    
+    func attack(_ opponent: RPGCharacter, damage: Int) -> RPGCharacter {
+        var damagedOpponent = opponent
+        damagedOpponent.receiveDamage(of: damage)
+        return damagedOpponent
+    }
+    
+    func heal(_ character: RPGCharacter, amount: Int) -> RPGCharacter {
+        var healedCharacter = character
+        healedCharacter.receiveHealing(of: amount)
+        return healedCharacter
     }
 }
 
@@ -26,7 +50,10 @@ class CharacterTests: XCTestCase {
     override func setUp() {
         sut = RPGCharacter()
     }
-    
+}
+
+// MARK:- Iteration 1
+extension CharacterTests {
     func testCharacter_onInit_healthIs1000() {
         XCTAssertEqual(sut.health, 1000)
     }
@@ -37,5 +64,71 @@ class CharacterTests: XCTestCase {
     
     func testCharacter_onInit_isAlive() {
         XCTAssertTrue(sut.isAlive)
+    }
+    
+    func testCharacter_receiveDamage_substractHealthEqualToDamageReceived() {
+        givenCharacter(initialHealth: 1000)
+        whenCharacterReceivesDamage(amount: 100)
+        thenCharacterHealthIs(900)
+    }
+    
+    func testCharater_receiveDamageSuperiorToHealth_characterHealthIsZero() {
+        givenCharacter(initialHealth: 1000)
+        whenCharacterReceivesDamage(amount: 2000)
+        thenCharacterHealthIs(0)
+    }
+    
+    func testCharacter_receiveDamageSuperiorToHealth_characterIsDead() {
+        givenCharacter(initialHealth: 1000)
+        whenCharacterReceivesDamage(amount: 1000)
+        thenCharacterIsDead()
+    }
+    
+    func testCharacter_dealsDamage() {
+        let attacker = RPGCharacter()
+        let opponent = RPGCharacter(health: 1000)
+        let damagedOponent = attacker.attack(opponent, damage: 100)
+        XCTAssertEqual(damagedOponent.health, 900)
+    }
+    
+    func testCharacter_receivesHealing_addsHealingAmountToActualHealth() {
+        givenCharacter(initialHealth: 500)
+        whenCharacterReceivesHealing(of: 400)
+        thenCharacterHealthIs(900)
+    }
+    
+    func testCharacter_receivesHealing_healthCannotGoBeyond1000() {
+        givenCharacter(initialHealth: 900)
+        whenCharacterReceivesHealing(of: 200)
+        thenCharacterHealthIs(1000)
+    }
+    
+    func testCharacter_healsAnotherCharacter() {
+        let healer = RPGCharacter()
+        let hurtCharacter = RPGCharacter(health: 100)
+        let healedCharacter = healer.heal(hurtCharacter, amount: 600)
+        XCTAssertEqual(healedCharacter.health, 700)
+    }
+}
+
+private extension CharacterTests {
+    func givenCharacter(initialHealth: Int) {
+        sut = RPGCharacter(health: initialHealth)
+    }
+    
+    func whenCharacterReceivesDamage(amount: Int) {
+        sut.receiveDamage(of: amount)
+    }
+    
+    func whenCharacterReceivesHealing(of amount: Int) {
+        sut.receiveHealing(of: amount)
+    }
+    
+    func thenCharacterHealthIs(_ value: Int) {
+        XCTAssertEqual(sut.health, value)
+    }
+    
+    func thenCharacterIsDead() {
+        XCTAssertFalse(sut.isAlive)
     }
 }
